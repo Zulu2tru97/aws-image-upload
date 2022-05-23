12432ed10dd6c64264a9514b2a2b61cf8ab1a28c
Zulu2tru97 <wilk.s.0100@gmail.com>
2022-05-23 12:58:09 -0400

    start from amigos code

---
+++ src/main/java/com/manifest/awsimageupload/AwsImageUploadApplication.java
@@ -0,0 +1,13 @@
+package com.manifest.awsimageupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manifest/awsimageupload/AwsImageUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manifest.awsimageupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
