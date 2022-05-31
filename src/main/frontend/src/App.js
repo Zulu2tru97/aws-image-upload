a1a4f468c7049c6883d3c0615dc04a9d163a48a3
Zulu2tru7 <wilk.s.0100@gmail.com>
2022-05-31 12:27:18 -0400

    FrontEnd: added frontend and fixed credentials

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+/* eslint-disable no-undef */
+import React, {useState,useEffect,useCallback} from 'react';
 import './App.css';
+import axios from 'axios';
+import {useDropzone} from 'react-dropzone'
+
+const UserProfiles = () => {
+
+  const [userProfiles,setUserProfiles] = useState([]);
+
+  const fetchUserProfiles =  () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then((response) => {
+      console.log(response);
+      setUserProfiles(response.data);
+    });
+  }
+
+  useEffect(() => {
+    fetchUserProfiles();
+  },[]);
+
+
+  return userProfiles.map((userProfile,index) => {
+    return (
+      <div key={index}>
+        <br/>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+        {/* <h1>{userProfile.userProfileImageLink}</h1> */}
+        <Dropzone/>
+        <br/>
+      </div>
+    );
+  });
+
+};
+
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop 4 da bois ...</p> :
+          <p>Drag 'n' drop some images hoe , or click to select files</p>
+      }
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <UserProfiles/>
+     shit
     </div>
   );
 }
--- src/main/java/com/manifest/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/manifest/awsimageupload/config/AmazonConfig.java
@@ -3,21 +3,23 @@ package com.manifest.awsimageupload.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.nio.file.Path;
+
 @Configuration
 public class AmazonConfig {
 
+    private EnvironmentVariableCredentialsProvider environmentVariableCredentialsProvider;
+
     @Bean
     public AmazonS3 s3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "",
-                ""
-        );
+        AWSCredentials awsCredentials = environmentVariableCredentialsProvider.getCredentials();
         return AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
